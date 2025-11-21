751ac0d4a2e1736cacec0a8baa2a78d551279a97
wendy1123 <wendy.m123.m1@gmail.com>
2025-11-21 01:33:42 +0000

    Finished timestamp microservice

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
