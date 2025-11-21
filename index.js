ca9c40e68248113c8af460aed83e3b383983ad21
wendy1123 <wendy.m123.m1@gmail.com>
2025-11-21 16:31:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({optionsSuccessStatus: 200}));  
 
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: 'hello API' });
 });
-
 
 app.get("/api/:date?", (req, res) => {
   const dateParam = req.params.date;
   let date;
 
-  
- if (/^\d+$/.test(dateParam)) {
-  date = new Date(parseInt(dateParam));
-} else {
-   
-     if (/^\d+$/.test(dateParam)) {
-      date = new Date(timestamp);
+  if (!dateParam) {
+    date = new Date();
+  } else {
+    if (/^\d+$/.test(dateParam)) {
+      date = new Date(parseInt(dateParam));
     } else {
       date = new Date(dateParam);
     }
   }
 
   if (date.toString() === "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    });
+    return res.json({ error: "Invalid Date" });
   }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
-
-var listener = app.listen(process.env.PORT || 5000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 5000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
 });
